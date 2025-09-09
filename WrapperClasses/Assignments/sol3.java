3c6cd7e936ca39b5e51ea36a5277fd40fb84c8d6
Supriya201104 <supriyamakineni04@gmail.com>
2025-09-09 19:33:05 +0530

    Create sol3.java

---
+++ WrapperClasses/Assignments/sol3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Solution3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+
+        String binary = Integer.toBinaryString(num);
+        binary = String.format("%8s", binary).replace(' ', '0');
+
+        System.out.println("Binary representation: " + binary);
+
+        sc.close();
+    }
+}
