d656178d12a8244565d45db835c964912458ace5
Supriya201104 <supriyamakineni04@gmail.com>
2025-09-09 19:47:49 +0530

    Create StudentService.java

---
+++ Absraction_packages/MiniProject/mile1/service/StudentService.java
@@ -0,0 +1,40 @@
+package abstraction_packages_exception_handling.miniproject.com.mile1.service;
+
+import abstraction_packages_exception_handling.miniproject.com.mile1.bean.Student;
+
+public class StudentService {
+    public int findNumberOfNullMarksArray ( Student s[] )
+    {
+        try {
+
+        } 
+        catch(NullPointerException e ) {
+
+        }
+        return 0;
+    }    
+
+    public int findNumberOfNullName( Student s[] )
+    {
+        try {
+
+        } 
+        catch(NullPointerException e ) {
+
+        }
+        return 0;   
+    }
+
+    public int findNumberOfNullObjects ( Student s[] ) 
+    {
+        try {
+
+        } 
+        catch(NullPointerException e ) {
+
+        }
+        return 0;
+        
+    }
+
+}
